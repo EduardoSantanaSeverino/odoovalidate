b601c9bf559d02a0e863acab3d7ebf7fa97189fc
EduardoSantanaSeverino <eduardoamparos@gmail.com>
2020-08-03 17:26:36 -0400

    Added permissions to public

--- src/routes/web.php
+++ src/routes/web.php
@@ -14,3 +14,53 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+Route::get('home','HomeController@index');
+Route::get('ejemplo1','HomeController@index');
+Route::get('/','HomeController@index');
+
+Route::get('/', function () {
+    return view('home.index');
+});
+
+Route::get('ejemplo', function () {
+    return view('home.ejemplo1');
+});
+
+Route::get('ejemplo', function () {
+    return view('home.ejemplo1');
+});
+
+Route::get('/', function () {
+    return view('home.welcome');
+});
+Route::resource('home','HomeController');
+// Para Desabilitar la gestion de creacion de usuarios.
+Route::get('register', 'Auth\AuthController@showRegistrationForm');
+Route::post('register', 'Auth\AuthController@register');
+
+*/
+
+Route::get('/','HomeController@index');
+
+Route::get('home','HomeController@index');
+
+Route::get('atentados','AtentadosController@index');
+
+Route::get('validarcliente','ClientesJsonController@validarcliente');
+
+Route::resource('clientes','ClientesController');
+
+Route::resource('usuarios','UsuariosController');
+
+Route::auth();
